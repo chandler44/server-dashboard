602a0f5335c0624a5982e973fa15673aded2716a
chandler44 <changlover23@gmail.com>
2026-06-28 17:38:22 -0700

    Move HTML into template

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 import socket
+import platform
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"""
-    <h1>🚀 Chan's DevOps Dashboard</h1>
-    <p><strong>Status:</strong> Running ✅</p>
-    <p><strong>Hostname:</strong> {socket.gethostname()}</p>
-    <p><strong>Time:</strong> {datetime.now()}</p>
-    """
+    hostname = socket.gethostname()
+    current_time = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
+    operating_system = platform.system()
+    python_version = platform.python_version()
+
+    return render_template(
+        "index.html",
+        hostname=hostname,
+        current_time=current_time,
+        operating_system=operating_system,
+        python_version=python_version
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
